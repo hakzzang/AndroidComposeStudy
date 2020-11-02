14cdeb29c6f2b960d7649a738f3b2d1f632d2f67
hakzzang <mbh930312@naver.com>
2020-11-02 14:07:48 +0900

    Chapter1 코드 추가

--- app/src/main/java/com/hbs/composestudy/MainActivity.kt
+++ app/src/main/java/com/hbs/composestudy/MainActivity.kt
@@ -6,7 +6,12 @@ import androidx.compose.foundation.Text
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.setContent
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
 import androidx.ui.tooling.preview.Preview
 import com.hbs.composestudy.ui.ComposeStudyTheme
 
@@ -26,13 +31,21 @@ class MainActivity : AppCompatActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Text(text = "Hello1 $name!", style = ColorTextStyle(Color.Black))
+    Text(text = "Hello2 $name!", style = ColorTextStyle(Color.Yellow))
+    Text(text = "Hello3 $name!", style = ColorTextStyle(Color.Red))
+    Text(text = "Hello4 $name!", style = ColorTextStyle(Color.Gray))
+}
+
+@Composable
+fun ColorTextStyle(color: Color): TextStyle {
+    return TextStyle(color = color, fontSize = 16.sp)
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     ComposeStudyTheme {
-        Greeting("Android")
+        Greeting("감자튀김!")
     }
 }
